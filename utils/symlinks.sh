41bd66e62af2571d37793a9e3d66b6dc5b2b0405
Knut Synstad <knut@synstad.net>
2026-09-22 22:28:55 -0700

    updates

--- utils/symlinks.sh
+++ utils/symlinks.sh
@@ -17,7 +17,6 @@ echo "Creating symlinks ..."
 DOTFILES="$HOME/dotfiles"
 
 mkdir -p "$HOME/.config"
-mkdir -p "$HOME/.local/share/applications"
 
 
 # Config directories
@@ -29,29 +28,6 @@ ln -sfn "$DOTFILES/.config/rofi" "$HOME/.config/rofi"
 ln -sfn "$DOTFILES/.config/waybar" "$HOME/.config/waybar"
 ln -sfn "$DOTFILES/.config/qt6ct" "$HOME/.config/qt6ct"
 ln -sfn "$DOTFILES/.config/Kvantum" "$HOME/.config/Kvantum"
-
-# Application launchers
-
-ln -sf "$DOTFILES/.local/share/applications/steam.desktop" \
-  "$HOME/.local/share/applications/steam.desktop"
-
-ln -sf "$DOTFILES/.local/share/applications/discord.desktop" \
-  "$HOME/.local/share/applications/discord.desktop"
-
-ln -sf "$DOTFILES/.local/share/applications/1password.desktop" \
-  "$HOME/.local/share/applications/1password.desktop"
-
-ln -sf "$DOTFILES/.local/share/applications/firefox.desktop" \
-  "$HOME/.local/share/applications/firefox.desktop"
-
-ln -sf "$DOTFILES/.local/share/applications/gimp.desktop" \
-  "$HOME/.local/share/applications/gimp.desktop"
-
-ln -sf "$DOTFILES/.local/share/applications/scribus.desktop" \
-  "$HOME/.local/share/applications/scribus.desktop"
-
-ln -sf "$DOTFILES/.local/share/applications/spotify.desktop" \
-  "$HOME/.local/share/applications/spotify.desktop"
 
 
 # Shell
